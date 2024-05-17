2d1cf7684d5e754f658c49bb71270e93d19a21dc
AMAN RAJ <AMAN RAJ@LAPTOP-UD0GH7J4>
2024-05-17 13:11:43 +0530

    first push

---
+++ com.vtiger/src/test/java/pac1/ContactTest.java
@@ -0,0 +1,24 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createContactTest() {
+		String url = System.getProperty("url");
+		String browser = System.getProperty("browser" , "chrome");
+		String username = System.getProperty("username");
+		String password = System.getProperty("password");
+		
+		System.out.println(url);
+		System.out.println(browser);
+		System.out.println(username);
+		System.out.println(password);
+		System.out.println("execute createcontactTest");
+	}
+	@Test
+	public void modifycontactTest() {
+		System.out.println("execute modifycontactTest");
+	}
+
+}
